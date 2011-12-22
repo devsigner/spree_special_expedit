bd15b0cc6af66d0f4cd1dd0696b7aa3fa1d2a277
Cedric Darricau <cedricdarricau@gmail.com>
2011-12-22 18:27:33 +0100

    update design presentation

--- app/overrides/product_show.rb
+++ app/overrides/product_show.rb
@@ -1,23 +1,23 @@
 Deface::Override.new(
-  :virtual_path => "products/show",
-  :insert_after => "div#main-image",
-  :partial  => '../views/overrides/product_ask_sample_button',
   :name => "product_show1",
+  :virtual_path => "products/show",
+  :insert_after => "div#cart-form",
+  :partial  => '../views/overrides/product_ask_sample_button',
   :disabled => false
 )
 
 Deface::Override.new(
+  :name => "product_show2",
   :virtual_path => "products/_cart_form",
   :insert_after => "span.price",
   :partial  => '../views/overrides/product_show_sell_mode',
-  :name => "product_show2",
   :disabled => false
 )
 
-#Deface::Override.new(
-#  :virtual_path => "products/_cart_form",
-#  :replace => "code[erb-silent]:contains('@product.has_variants?')",
-#  :text => '&& !@product.has_sample',
-#  :name => "product_show3",
-#  :disabled => false
-#)
+Deface::Override.new(
+  :name => "product_show3",
+  :virtual_path => "products/_cart_form",
+  :replace => "span.sell_mode",
+  :partial  => '../views/overrides/product_show_sell_mode',
+  :disabled => false
+)
