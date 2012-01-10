645fab4a1e8a3170ef5d51f3c206b0129d9648b8
Cedric Darricau <cedricdarricau@gmail.com>
2012-01-10 15:53:11 +0100

    rename deflate for more comprehensive action

--- app/overrides/admin_product_form.rb
+++ app/overrides/admin_product_form.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   :virtual_path => "admin/products/_form",
   :insert_bottom => "[data-hook='admin_product_form_right']",
   :partial  => '../views/overrides/admin_product_form',
-  :name => "admin_product_form",
+  :name => "add_admin_product_form_sample",
   :disabled => false
 )
--- app/overrides/product_show.rb
+++ app/overrides/product_show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  :name => "product_show1",
+  :name => "product_ask_sample_button",
   :virtual_path => "products/show",
   :insert_after => "div#cart-form",
   :partial  => '../views/overrides/product_ask_sample_button',
@@ -7,7 +7,7 @@ Deface::Override.new(
 )
 
 Deface::Override.new(
-  :name => "product_show2",
+  :name => "product_show_sell_mode",
   :virtual_path => "products/_cart_form",
   :insert_after => "span.price",
   :partial  => '../views/overrides/product_show_sell_mode',
@@ -15,7 +15,7 @@ Deface::Override.new(
 )
 
 Deface::Override.new(
-  :name => "product_show3",
+  :name => "product_show_sell_mode_replace_sell_mode",
   :virtual_path => "products/_cart_form",
   :replace => "span.sell_mode",
   :partial  => '../views/overrides/product_show_sell_mode',
